c2ac603cd366c1ce7017bd1d0500c2e9b247c666
Vladimir Podolinsky <vpodolinskiy@mail.ru>
2019-04-05 18:16:41 +0300

    Add drawing tieangle program

--- TestProjectN1/Main.cpp
+++ TestProjectN1/Main.cpp
@@ -38,14 +38,23 @@ int main(int argc, char* argv[])
 	glViewport(0, 0, 800, 600);
 
 	float vertices[] = {
-		-0.5f, -0.5f,  0.0f,
-		 0.5f, -0.5f,  0.0f,
+		-0.7f, -0.3f,  0.0f,
+		 0.9f, -0.1f,  0.0f,
 		 0.0f,  0.5f,  0.0f
 	};
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(VAO);
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -77,7 +86,21 @@ int main(int argc, char* argv[])
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 	
-	
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER_PROGRAM::LINK::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	// Rendering cycle.
 	while(!glfwWindowShouldClose(window))
 	{
@@ -87,6 +110,9 @@ int main(int argc, char* argv[])
 		glfwPollEvents();
 		glClearColor(0.1f, 0.05f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 	}
 	glfwTerminate();
 
